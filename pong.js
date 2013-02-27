88d9a3f33fe78006e522fa92a8a7c58b214297ac
Gregory Schier <gschier1990@gmail.com>
2013-02-27 01:27:12 -0800

    Working demo (simulation only so far)

---
+++ pong.js
@@ -0,0 +1,9 @@
+
+$( function() {
+  var canvas = document.getElementById('pong');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    var game = new Game(5, ctx, canvas);
+  }
+});
+
